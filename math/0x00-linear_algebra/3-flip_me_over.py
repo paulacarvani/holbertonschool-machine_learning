c76eef6b326ab1b8b0c99218a8ce0636c2c20bd0
paulacarvani <3312@holbertonschool.com>
2022-07-26 10:46:50 -0500

    Task 3 - Write a function def matrix_transpose(matrix): that returns the transpose of a 2D matrix

---
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Write a function def matrix_transpose(matrix):
+that returns the transpose of a 2D matrix, matrix:
+
+You must return a new matrix
+You can assume that matrix is never empty
+You can assum all elements in the same dimension are of the same type/shape"""
+
+
+def matrix_transpose(matrix):
+    m2 = []
+    for i in range(len(matrix[0])):
+        row = []
+        for item in matrix:
+            row.append(item[i])
+        m2.append(row)
+    return m2
