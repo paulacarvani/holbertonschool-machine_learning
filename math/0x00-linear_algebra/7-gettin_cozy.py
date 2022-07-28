04b966b3f3938d125a39355f394221ebb38dfb91
paulacarvani <3312@holbertonschool.com>
2022-07-27 21:28:56 -0500

    Task 7. Gettin’ Cozy - Test Checkers

---
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""Write a function def cat_matrices2D(mat1, mat2, axis=0):
+that concatenates two matrices along a specific axis:
+
+You can assume that mat1 and mat2 are 2D matrices containing ints/floats
+You can assume all elements in the same dimension are of the same type/shape
+You must return a new matrix
+If the two matrices cannot be concatenated, return None"""
+
+
+def is_list(matrix):
+    """ returns True if the matrix is a list """
+    return type(matrix) is list
+
+
+def matrix_shape(matrix):
+    """ Returns the shape of the matrix"""
+    if type(matrix) != list or not matrix:
+        return [0, ]
+    return [len(matrix), *matrix_shape(matrix[0])]
+
+
+def equal_without(matrix1, matrix2, without=0, index=0):
+    """Returns true if the two matrices are equal without any index"""
+
+    if without != -1:
+        del matrix1[without]
+        del matrix2[without]
+        without = -1
+
+    if index >= len(matrix1):
+        return True
+
+    try:
+        if matrix1[index] != matrix2[index]:
+            return False
+    except IndexError:
+        return True
+
+    return equal_without(matrix1, matrix2, without, index + 1)
+
+
+def cat_matrices2D(mat1, mat2, axis=0, firts=True):
+    """ Concat matrices to a single matrix """
+    if not is_list(mat1) and not is_list(mat2):
+        return None
+
+    shape_one = matrix_shape(mat1)
+    shape_two = matrix_shape(mat2)
+    if firts and not equal_without(shape_one, shape_two, axis):
+        return None
+
+    if (axis == 0):
+        return [*mat1, *mat2]
+
+    result = list(range(len(mat1)))
+    for i in range(len(mat1)):
+        result[i] = cat_matrices2D(mat1[i], mat2[i], axis - 1, False)
+    return result
