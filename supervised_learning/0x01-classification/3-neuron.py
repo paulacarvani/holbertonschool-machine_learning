26a247c7448b6818269e5e5fd80e04c751591e42
paulacarvani <3312@holbertonschool.com>
2022-08-23 16:57:45 -0500

    Task 3. Neuron Cost

---
+++ supervised_learning/0x01-classification/3-neuron.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""Based on 2-neuron.py
+Add the public method def cost(self, Y, A):
+Calculates the cost of the model using logistic regression
+Y is a numpy.ndarray with shape (1, m) that contains the correct
+labels for the input data
+A is a numpy.ndarray with shape (1, m) containing the activated
+output of the neuron for each example
+To avoid division by zero errors, please use 1.0000001 - A instead of 1 - A
+Returns the cost"""
+
+import numpy as np
+
+
+def sigmoid(y):
+    """Sigmoid Activation Function"""
+    return 1/(1+np.exp(-y))
+
+
+class Neuron:
+    """That defines a single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """class constructor"""
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    """Each private attribute should have a corresponding
+    getter function (no setter function)"""
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
+
+    def forward_prop(self, X):
+        self.__A = sigmoid(np.matmul(self.__W, X) + self.__b)
+        return self.__A
+
+    def cost(self, Y, A):
+        cost = Y * np.log(A) + np.log(1.0000001 - A) * (1-Y)
+        return (-cost.sum() / len(np.transpose(Y)))
