af97c3d9066f55a746610588407de775c9f62572
paulacarvani <3312@holbertonschool.com>
2022-08-23 19:53:03 -0500

    Task 9. Privatize NeuralNetwork

---
+++ supervised_learning/0x01-classification/9-neural_network.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""class NeuralNetwork that defines a neural
+network with one hidden layer performing binary classification"""
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """neural network"""
+
+    def __init__(self, nx, nodes):
+        """class constructor"""
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        if type(nodes) != int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        # Private instance attributes
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros(nodes).reshape(nodes, 1)
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+        # Each private attribute should have a corresponding getter function
+    @property
+    def W1(self):
+        return self.__W1
+
+    @property
+    def b1(self):
+        return self.__b1
+
+    @property
+    def A1(self):
+        return self.__A1
+
+    @property
+    def W2(self):
+        return self.__W2
+
+    @property
+    def b2(self):
+        return self.__b2
+
+    @property
+    def A2(self):
+        return self.__A2
