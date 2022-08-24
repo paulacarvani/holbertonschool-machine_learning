40d4126cb4d12605a7157667d5abb1ed1a9fcbc7
paulacarvani <3312@holbertonschool.com>
2022-08-23 21:41:10 -0500

    Task 16. DeepNeuralNetwork

--- supervised_learning/0x01-classification/16-deep_neural_network.py
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -13,21 +13,22 @@ class DeepNeuralNetwork:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if type(layers) != list:
+        if type(layers) != list or len(layers) < 1:
             raise TypeError("layers must be a list of positive integers")
-        self.nx = nx
-        self.layers = layers
+
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
-        for i in range(len(layers)):
-            if not isinstance(layers[i], int) or layers[i] <= 0:
-                raise TypeError("layers must be a list of positive integers")
-            if i == 0:
-                self.weights['W' + str(i + 1)] = np.random.normal(
-                    layers[i], nx) * np.sqrt(2 / nx)
+
+        for i in range(self.L):
+            if type(layers[i]) != int or layers[i] < 0:
+                raise TypeError('layers must be a list of positive integers')
+            kb = "b" + str(i + 1)
+            kW = "W" + str(i + 1)
+
+            self.weights[kb] = np.zeros(layers[i]).reshape(layers[i], 1)
+            if i > 0:
+                aux = layers[i-1]
             else:
-                self.weights['W' + str(i+1)] = np.random.randn(
-                    layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
-            self.weights['b' + str(i + 1)] = np.zeros(
-                layers[i]).reshape(layers[i], 1)
+                aux = nx
+            self.weights[kW] = np.random.randn(layers[i], aux) * np.sqrt(2/aux)
