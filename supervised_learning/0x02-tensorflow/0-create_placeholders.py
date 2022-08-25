c1782cbd1237416d082755ed7751386241a5ab8e
paulacarvani <3312@holbertonschool.com>
2022-08-25 14:35:35 -0500

    Task 0. Placeholders

--- supervised_learning/0x02-tensorflow/0-create_placeholders.py
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -2,11 +2,11 @@
 """Write the function def create_placeholders(nx, classes):
 that returns two placeholders, x and y, for the neural network:"""
 
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 
 
 def create_placeholders(nx, classes):
     """returns two placeholders, x and y, for the neural network"""
-    x = tf.placeholder("float", [None], nx, name="x")
-    y = tf.placeholder("float", [None, classes], name="y")
+    x = tf.placeholder(tf.float32, [None, nx], name="x")
+    y = tf.placeholder(tf.float32, [None, classes], name="y")
     return x, y
