8eb64cfec7137d9ab1472dc59a31c00b8b33fb9f
paulacarvani <3312@holbertonschool.com>
2022-08-25 21:30:05 -0500

    Task 7

---
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Defines `evaluate`."""
+import tensorflow.compat.v1 as tf
+
+
+def evaluate(X, Y, save_path):
+    """
+    Evaluates the output of a neural network.
+    Args:
+        X: A numpy.ndarray containing the input data to evaluate.
+        Y: A numpy.ndarray containing the one-hot labels for X.
+        save_path: The location to load the model from.
+    Returns: the network's prediction, accuracy, and loss, respectively
+    """
+    with tf.Session() as session:
+        saver = tf.train.import_meta_graph(save_path + ".meta")
+        saver.restore(session, save_path)
+
+        x = tf.get_collection("x")[0]
+        y = tf.get_collection("y")[0]
+        y_pred = tf.get_collection("y_pred")[0]
+        accuracy = tf.get_collection("accuracy")[0]
+        loss = tf.get_collection("loss")[0]
+
+        routine = (y_pred, accuracy, loss)
+        return session.run(routine, {x: X, y: Y})
