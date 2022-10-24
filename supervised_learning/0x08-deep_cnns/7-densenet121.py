3e9b2453ee360b5a173f8349c9ab4600981ee1c6
paulacarvani <3312@holbertonschool.com>
2022-10-24 16:04:43 -0500

    New corrections

--- supervised_learning/0x08-deep_cnns/7-densenet121.py
+++ supervised_learning/0x08-deep_cnns/7-densenet121.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-fn builds the DenseNet-121 architecture
+Builds a DenseNet-121 architecture
 """
 import tensorflow.keras as K
 dense_block = __import__('5-dense_block').dense_block
@@ -9,32 +9,55 @@ transition_layer = __import__('6-transition_layer').transition_layer
 
 def densenet121(growth_rate=32, compression=1.0):
     """
-    growth_rate is the growth rate
-    compression is the compression factor
-    You can assume the input data will have shape (224, 224, 3)
-    All convolutions should be preceded by Batch Normalization and a
-        rectified linear activation (ReLU), respectively
-    All weights should use he normal initialization
-    You may use:
-        dense_block = __import__('5-dense_block').dense_block
-        transition_layer = __import__('6-transition_layer').transition_layer
-    Returns: the keras model
+    a function that builds a DenseNet-121 architecture
+    :param growth_rate: is the growth rate for the dense block
+    :param compression: is the compression factor for the transition layer
+    :return: The keras model
     """
-    input_Data = K.Input(shape=(224, 224, 3))
-    init = K.initializers.he_normal(seed=None)
-    r = K.layers.BatchNormalization()(input_Data)
-    r = K.layers.Activation('relu')(r)
-    r = K.layers.Conv2D(growth_rate * 2, 7, 2, padding='same',
-                        kernel_initializer=init)(r)
-    r = K.layers.MaxPool2D(2)(r)
-    r, f = dense_block(r, growth_rate * 2, growth_rate, 6)
-    r, f = transition_layer(r, f, compression)
-    r, f = dense_block(r, f, growth_rate, 12)
-    r, f = transition_layer(r, f, compression)
-    r, f = dense_block(r, f, growth_rate, 24)
-    r, f = transition_layer(r, f, compression)
-    r, f = dense_block(r, f, growth_rate, 16)
-    r = K.layers.AvgPool2D(7)(r)
-    r = K.layers.Dense(1000, kernel_initializer=init,
-                       activation='softmax')(r)
-    return K.Model(input_Data, r)
+    init = K.initializers.he_normal()
+    X = K.Input(shape=(224, 224, 3))
+    n_filters = 2 * growth_rate
+
+    norm1 = K.layers.BatchNormalization()(X)
+    act1 = K.layers.Activation("relu")(norm1)
+    conv1 = K.layers.Conv2D(filters=n_filters, kernel_size=(7, 7),
+                            padding="same",
+                            strides=(2, 2),
+                            kernel_initializer=init)(act1)
+
+    max_pool = K.layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2),
+                                     padding="same")(conv1)
+
+    # First block
+    dense_block1, n_filters = dense_block(max_pool, n_filters, growth_rate, 6)
+
+    transition_layer1, n_filters = transition_layer(dense_block1, n_filters,
+                                                    compression)
+
+    # Second block
+    dense_block2, n_filters = dense_block(transition_layer1, n_filters,
+                                          growth_rate, 12)
+
+    transition_layer2, n_filters = transition_layer(dense_block2, n_filters,
+                                                    compression)
+
+    # Third Block
+    dense_block3, n_filters = dense_block(transition_layer2, n_filters,
+                                          growth_rate, 24)
+
+    transition_layer3, n_filters = transition_layer(dense_block3, n_filters,
+                                                    compression)
+
+    # Last block (no transition)
+    dense_block4, n_filters = dense_block(transition_layer3, n_filters,
+                                          growth_rate, 16)
+
+    avg_pool = K.layers.AveragePooling2D(pool_size=(7, 7), strides=(1, 1),
+                                         padding="valid")(dense_block4)
+
+    softmax = K.layers.Dense(units=1000, activation="softmax",
+                             kernel_initializer=init)(avg_pool)
+
+    model = K.Model(inputs=X, outputs=softmax)
+
+    return model
