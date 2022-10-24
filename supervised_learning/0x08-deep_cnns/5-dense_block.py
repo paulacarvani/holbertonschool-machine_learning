7300b5bb571f10d06c11277479c94c6efc591917
paulacarvani <3312@holbertonschool.com>
2022-10-24 16:02:58 -0500

    New corrections

--- supervised_learning/0x08-deep_cnns/5-dense_block.py
+++ supervised_learning/0x08-deep_cnns/5-dense_block.py
@@ -1,43 +1,38 @@
 #!/usr/bin/env python3
 """
-fn builds dense block
+Builds a dense block
 """
-
-
 import tensorflow.keras as K
 
 
 def dense_block(X, nb_filters, growth_rate, layers):
     """
-    X is the output from the previous layer
-    nb_filters is an integer representing the number of filters in X
-    growth_rate is the growth rate for the dense block
-    layers is the number of layers in the dense block
-    You should use the bottleneck layers used for DenseNet-B
-    All weights should use he normal initialization
-    All convolutions should be preceded by Batch Normalization and
-        a rectified linear activation (ReLU), respectively
-    Returns: The concatenated output of each layer within the
-        Dense Block and the number of filters within the
-        concatenated outputs, respectively
+    a function that builds a dense block
+    :param X: is the output from the previous layer
+    :param nb_filters: is an integer representing the number of filters in X
+    :param growth_rate: is the growth rate for the dense block
+    :param: layers: is the number of layers in the dense block
+    :return: The concatenated output of each layer within the Dense Block and
+    the number of filters within the concatenated outputs, respectively
     """
+    init = K.initializers.he_normal()
 
-    init = K.initializers.he_normal(seed=None)
+    for i in range(layers):
+        norm0 = K.layers.BatchNormalization()(X)
+        act0 = K.layers.Activation("relu")(norm0)
+        bottle = K.layers.Conv2D(filters=4*growth_rate, kernel_size=(1, 1),
+                                 padding="same",
+                                 strides=(1, 1),
+                                 kernel_initializer=init)(act0)
 
-    for blocks in range(layers):
-        batchNorm_1 = K.layers.BatchNormalization()(X)
-        activation_1 = K.layers.Activation('relu')(batchNorm_1)
-        cnn_1x1 = K.layers.Conv2D(filters=growth_rate * 4,
-                                  kernel_size=1,
-                                  padding='same',
-                                  kernel_initializer=init)(activation_1)
-        batchNorm_2 = K.layers.BatchNormalization()(cnn_1x1)
-        activation_2 = K.layers.Activation('relu')(batchNorm_2)
-        # cnn 3x3
-        next_X = K.layers.Conv2D(filters=growth_rate,
-                                 kernel_size=3,
-                                 padding='same',
-                                 kernel_initializer=init)(activation_2)
-        X = K.layers.concatenate([X, next_X])
+        norm1 = K.layers.BatchNormalization()(bottle)
+        act1 = K.layers.Activation("relu")(norm1)
+        conv = K.layers.Conv2D(filters=growth_rate, kernel_size=(3, 3),
+                               padding="same",
+                               strides=(1, 1),
+                               kernel_initializer=init)(act1)
+
+        X = K.layers.concatenate([X, conv])
         nb_filters += growth_rate
-    return X,
+
+    return X, nb_filters
