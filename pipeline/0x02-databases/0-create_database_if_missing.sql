5aa1f35238a9a68ddb8cbe4c013aa1033c4f8cac
paulacarvani <3312@holbertonschool.com>
2023-03-28 22:02:28 -0500

    first commit

---
+++ pipeline/0x02-databases/0-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+/* Write a script that creates the database db_0 in your MySQL server.
+If the database db_0 already exists, your script should not fail
+You are not allowed to use the SELECT or SHOW statements */
+CREATE DATABASE IF NOT EXISTS db_0;
