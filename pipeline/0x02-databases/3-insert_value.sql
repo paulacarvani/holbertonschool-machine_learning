8d7130867c1634418878fd736bde6251245acf1c
paulacarvani <3312@holbertonschool.com>
2023-03-28 22:14:06 -0500

    insert into

--- pipeline/0x02-databases/3-insert_value.sql
+++ pipeline/0x02-databases/3-insert_value.sql
@@ -1,2 +1,2 @@
 -- Write a script that inserts a new row in the table first_table in your MySQL server.
-INSERT INTO db_0.first_table (id, name) VALUES (89, "Holberton School");
+INSERT INTO first_table (id, name) VALUES (89, "Holberton School");
