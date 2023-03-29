550011264c28f08ba79da7d90b1ca0a7d840367e
paulacarvani <3312@holbertonschool.com>
2023-03-28 23:02:15 -0500

    task 18 - 34

---
+++ pipeline/0x02-databases/20-average_score.sql
@@ -0,0 +1,10 @@
+-- 20-average_score.sql
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id_new INTEGER)
+BEGIN
+	UPDATE users SET average_score=(
+	SELECT AVG(score) FROM corrections WHERE user_id=user_id_new)
+	WHERE id=user_id_new;
+END; //
+DELIMITER ;
