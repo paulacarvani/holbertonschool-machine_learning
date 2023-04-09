3495211713c83989a5911d52d6c8d54f8fb23a49
paulacarvani <3312@holbertonschool.com>
2023-04-09 18:45:18 -0500

    Task 3

---
+++ supervised_learning/0x0F-word_embeddings/3-gensim_to_keras.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Converts a gensim word2vec model to a keras Embedding layer
+"""
+from gensim.models import Word2Vec
+import tensorflow.keras as keras
+
+
+def gensim_to_keras(model):
+    """
+    Converts a gensim word2vec model to a keras Embedding layer
+    """
+    return model.wv.get_keras_embedding(train_embeddings=False)
